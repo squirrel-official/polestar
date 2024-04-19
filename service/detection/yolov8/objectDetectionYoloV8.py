5ce6c928061059f5495b666bbdceffaceb4386af
anil <anil.kumar.ait09@gmail.com>
2024-04-20 01:25:27 +1000

    Added YOLO

--- service/detection/yolov8/objectDetectionYoloV8.py
+++ service/detection/yolov8/objectDetectionYoloV8.py
@@ -7,15 +7,16 @@ model = YOLO('yolov8n.pt')  # You can choose other models like 'yolov8s.pt', 'yo
 
 def detect_objects(image):
     results = model(image)
-    print(results)
-    print(results.pandas())
-    if results.pandas().xyxy[0].shape[0] > 0:  # Check if any objects were detected
-        print('detected')
-        print(results)
-        for x_min, y_min, x_max, y_max, conf, name in results.pandas().xyxy[0].values:
+    objects_df = results.pandas().xyxy[0]
+    if objects_df.shape[0] > 0:  # Check if any objects were detected
+        print('Objects detected:')
+        print(objects_df)
+        for x_min, y_min, x_max, y_max, conf, name in objects_df.values:
             cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
             cv2.putText(image, f"{name} ({conf:.2f})", (int(x_min), int(y_min) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
                         (0, 255, 0), 2)
         return True
     else:
+        print('No objects detected.')
         return False
+
