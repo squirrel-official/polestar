e0367a51e36b2d734af447822bd5616203a9e4de
anil <anil.kumar.ait09@gmail.com>
2024-04-20 01:57:01 +1000

    Added YOLO

--- service/detection/yolov8/objectDetectionYoloV8.py
+++ service/detection/yolov8/objectDetectionYoloV8.py
@@ -6,6 +6,9 @@ model = YOLO('yolov8n.pt')  # You can choose other models like 'yolov8s.pt', 'yo
 
 
 def detect_objects(image):
+    prediction = model.predict(image)
+    print(prediction)
+    print(type(prediction))
     results = model(image)
     print('anil1')
     print(results)
