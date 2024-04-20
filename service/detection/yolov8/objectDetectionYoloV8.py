cc9b5643de8a77c5d7d9d26c60bc3f9b8c8eb815
anil <anil.kumar.ait09@gmail.com>
2024-04-21 01:38:59 +1000

    Added YOLO

--- service/detection/yolov8/objectDetectionYoloV8.py
+++ service/detection/yolov8/objectDetectionYoloV8.py
@@ -1,6 +1,6 @@
 import math
 from ultralytics import YOLO
-
+import cv2
 # Load the YOLOv8 model (replace with your desired model path)
 model = YOLO('yolov8n.pt')  # You can choose other models like 'yolov8s.pt', 'yolov8m.pt', etc.
 
@@ -9,15 +9,21 @@ def detect_objects(image):
     # in future this needs  to be done on stream so that the all results are produced continuously
     all_results = model(image, stream=True)
     for result in all_results:
+        print(result.boxes.xyxy)
+        boxes = result.boxes.xyxy
         classes = result.boxes.cls
         confidences = result.boxes.conf
         for i in range(len(classes)):
             class_type = classes[i]
             confidence = confidences[i]
             confidence = math.ceil(confidence * 100)
+            x_min, y_min, x_max, y_max = boxes[i]
+
             if confidence > 50:
                 print(class_type)
                 print(confidence)
+                cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
                 found = True
+
 
     return found
