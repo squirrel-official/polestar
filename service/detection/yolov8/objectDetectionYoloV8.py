f8c68e46f24cfa93b0b7db8615152b8cee44de7d
anil <anil.kumar.ait09@gmail.com>
2024-04-23 23:14:32 +1000

    detection logic not to send lot of notifications

--- service/detection/yolov8/objectDetectionYoloV8.py
+++ service/detection/yolov8/objectDetectionYoloV8.py
@@ -16,7 +16,7 @@ def detect_objects(image, timestamp, dir_path):
             class_type = classes[i]
             confidence = confidences[i]
             confidence = math.ceil(confidence * 100)
-            if class_type == 0 and confidence > 70:
+            if class_type == 0 and confidence > 50:
                 print("Human found,saving picture")
                 complete_file_name = dir_path + str(timestamp) + '.jpg'
                 result.save(filename=complete_file_name)
