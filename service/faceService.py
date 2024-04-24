4f3db7f00930785f0ad8010e5d1512e9f380a957
anil <anil.kumar.ait09@gmail.com>
2024-04-25 02:14:41 +1000

    debug

--- service/faceService.py
+++ service/faceService.py
@@ -38,9 +38,9 @@ def facial_comparison_checks(image):
 
     try:
         start_time = time.time()
-        unknown_faces = DeepFace.extract_faces(image, enforce_detection=True, detector_backend=backends[6])
+        unknown_faces = DeepFace.extract_faces(image, enforce_detection=True, detector_backend=backends[9])
         if unknown_faces is not None:
-            logger.debug('face extraction success , total time : {0}', str(time.time() - start_time))
+            logger.debug('face extraction success , total time : '+ str((time.time() - start_time)))
             for unknown_face in enumerate(unknown_faces):
                 # face tuple's 2nd  element has facial encodings
                 unknown_face_encoding = unknown_face[1]['face']
