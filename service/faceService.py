3d8eff4549cae3d68c3b6d5ba5f072a8e18a1edd
anil <anil.kumar.ait09@gmail.com>
2024-04-24 18:56:48 +1000

    Changed the logic to enforce face detection for face recognition

--- service/faceService.py
+++ service/faceService.py
@@ -8,11 +8,6 @@ MOTION_VIDEO_URL = '/var/lib/motion/*'
 CONFIG_PROPERTIES = '/usr/local/polestar/config.properties'
 ARCHIVE_URL = "/usr/local/polestar/data/archives/"
 
-# For writing
-UNKNOWN_VISITORS_PATH = '/usr/local/polestar/result/unknown-visitors/'
-CAPTURED_CRIMINALS_PATH = '/usr/local/polestar/result/captured-criminals/'
-KNOWN_VISITORS_PATH = '/usr/local/polestar/result/known-visitors/'
-
 # For reading
 WANTED_CRIMINALS_PATH = '/usr/local/polestar/data/wanted-criminals/'
 FAMILIAR_FACES_PATH = '/usr/local/polestar/data/familiar-faces/'
@@ -24,17 +19,22 @@ FRIEND_NOTIFICATION_URL = 'http://my-security.local:8087/friend'
 
 def facial_comparison_checks(image):
     detectors = ["opencv", "ssd", "mtcnn", "dlib", "retinaface"]
-    unknown_faces = DeepFace.extract_faces(image, enforce_detection=False)
-    if unknown_faces is not None:
-        logger.debug('A new person identified by face so processing it')
-        for unknown_face in enumerate(unknown_faces):
-            # face tuple's 2nd  element has facial encodings
-            unknown_face_encoding = unknown_face[1]['face']
+    try:
+        unknown_faces = DeepFace.extract_faces(image, enforce_detection=False)
+        if unknown_faces is not None:
+            logger.debug('A new person identified by face so processing it')
+            for unknown_face in enumerate(unknown_faces):
+                # face tuple's 2nd  element has facial encodings
+                unknown_face_encoding = unknown_face[1]['face']
 
-            criminal_result = DeepFace.find(img_path=unknown_face_encoding,
-                                            db_path=WANTED_CRIMINALS_PATH,enforce_detection=False)
-            print(criminal_result)
+                criminal_result = DeepFace.find(img_path=unknown_face_encoding,
+                                                db_path=WANTED_CRIMINALS_PATH,enforce_detection=False)
+                print(criminal_result)
 
-            friend_result = DeepFace.find(img_path=unknown_face_encoding,
-                                          db_path=FAMILIAR_FACES_PATH,enforce_detection=False)
-            print(friend_result)
+                friend_result = DeepFace.find(img_path=unknown_face_encoding,
+                                              db_path=FAMILIAR_FACES_PATH,enforce_detection=False)
+                print(friend_result)
+    except Exception as e:
+        logger.error(e)
+        logger.info('Unable to extract the face from passed frame')
+        pass
