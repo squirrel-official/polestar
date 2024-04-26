8edd2046cafd021db1ab7f3095ba51d6db6840a4
anil <anil.kumar.ait09@gmail.com>
2024-04-26 17:47:17 +1000

    Resolution

--- service/motionDetection.py
+++ service/motionDetection.py
@@ -25,7 +25,7 @@ def monitor_camera_stream():
         camera = Picamera2()
 
         # Configure the still capture stream (no preview)
-        config = camera.create_still_configuration(main={"size": (2560, 1440)})
+        config = camera.create_still_configuration(main={"size": (4056, 3040)})
         camera.configure(config)
 
         # Start the camera stream (without preview)
